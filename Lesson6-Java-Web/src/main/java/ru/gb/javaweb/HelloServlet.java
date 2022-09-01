9719b19f0647781500315506ef0b44fbc0b46792
Vera861 <mverynchik@gmail.com>
2022-09-02 00:46:59 +0300

    lesson6

---
+++ Lesson6-Java-Web/src/main/java/ru/gb/javaweb/HelloServlet.java
@@ -0,0 +1,26 @@
+package ru.gb.javaweb;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>" + message + "</h2>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ Lesson6-Java-Web/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "This is index page!" %></h1>
+<h1><%= "Это стартовая страница!" %></h1>
+<br/>
+<a href="hello">redirect to Hello Servlet</a>
+</body>
+</html>
